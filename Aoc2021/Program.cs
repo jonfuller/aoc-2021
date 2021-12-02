b7652138509b9cb1c27afe84f3d0d1ff53e3b856
Jon Fuller <jcfuller@sep.com>
2021-12-02 09:38:20 -0500

    initial commit * Part 1A working * added morelinq library

---
+++ Aoc2021/Program.cs
@@ -0,0 +1,12 @@
+﻿global using MoreLinq;
+
+var depthsSample = File.ReadAllLines("input/01_sample").Select(int.Parse);
+var depthsActual = File.ReadAllLines("input/01").Select(int.Parse);
+
+Console.WriteLine(CountIncreases(depthsSample));
+Console.WriteLine(CountIncreases(depthsActual));
+
+static int CountIncreases(IEnumerable<int> depths) => depths
+    .Pairwise((a, b) => b > a)
+    .Count(x => x);
+
