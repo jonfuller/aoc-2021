ae2b54fa0a623324ef93138d34eca2d42a3c6b0f
Jon Fuller <jcfuller@sep.com>
2021-12-02 11:57:00 -0500

    pull part 1 into it's own file
    * added command line arg library clifx

---
+++ Aoc2021/Day01.cs
@@ -0,0 +1,31 @@
+﻿namespace Aoc2021;
+
+[Command(name: "day-01")]
+public class Day01 : ICommand
+{
+    public async ValueTask ExecuteAsync(IConsole console)
+    {
+        var depthsSample = File.ReadAllLines("input/01_sample").Select(int.Parse);
+        var depthsActual = File.ReadAllLines("input/01").Select(int.Parse);
+
+        PartA();
+        PartB();
+
+        void PartA()
+        {
+            console.Output.WriteLine(CountWindowIncreases(1, depthsSample));
+            console.Output.WriteLine(CountWindowIncreases(1, depthsActual));
+        }
+
+        void PartB()
+        {
+            console.Output.WriteLine(CountWindowIncreases(3, depthsSample));
+            console.Output.WriteLine(CountWindowIncreases(3, depthsActual));
+        }
+
+        static int CountWindowIncreases(int windowSize, IEnumerable<int> depths) => depths
+            .Window(windowSize)
+            .Pairwise((a, b) => b.Sum() > a.Sum())
+            .Count(x => x);
+    }
+}
--- Aoc2021/Program.cs
+++ Aoc2021/Program.cs
@@ -1,24 +1,9 @@
 ﻿global using MoreLinq;
+global using CliFx;
+global using CliFx.Attributes;
+global using CliFx.Infrastructure;
 
-var depthsSample = File.ReadAllLines("input/01_sample").Select(int.Parse);
-var depthsActual = File.ReadAllLines("input/01").Select(int.Parse);
-
-PartA();
-PartB();
-
-void PartA()
-{
-    Console.WriteLine(CountWindowIncreases(1, depthsSample));
-    Console.WriteLine(CountWindowIncreases(1, depthsActual));
-}
-
-void PartB()
-{
-    Console.WriteLine(CountWindowIncreases(3, depthsSample));
-    Console.WriteLine(CountWindowIncreases(3, depthsActual));
-}
-
-static int CountWindowIncreases(int windowSize, IEnumerable<int> depths) => depths
-    .Window(windowSize)
-    .Pairwise((a, b) => b.Sum() > a.Sum())
-    .Count(x => x);
+await new CliApplicationBuilder()
+       .AddCommandsFromThisAssembly()
+       .Build()
+       .RunAsync();
