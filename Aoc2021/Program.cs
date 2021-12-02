8140667cd014827d29ae2f33008bd22f87d49ca5
Jon Fuller <jcfuller@sep.com>
2021-12-02 09:57:28 -0500

    day 1 - part 2
    * added checker for increases with window size
    * split into partA, partB methods

--- Aoc2021/Program.cs
+++ Aoc2021/Program.cs
@@ -3,10 +3,26 @@
 var depthsSample = File.ReadAllLines("input/01_sample").Select(int.Parse);
 var depthsActual = File.ReadAllLines("input/01").Select(int.Parse);
 
-Console.WriteLine(CountIncreases(depthsSample));
-Console.WriteLine(CountIncreases(depthsActual));
+PartA();
+PartB();
+
+void PartA()
+{
+    Console.WriteLine(CountIncreases(depthsSample));
+    Console.WriteLine(CountIncreases(depthsActual));
+}
+
+void PartB()
+{
+    Console.WriteLine(CountWindowIncreases(3, depthsSample));
+    Console.WriteLine(CountWindowIncreases(3, depthsActual));
+}
 
 static int CountIncreases(IEnumerable<int> depths) => depths
     .Pairwise((a, b) => b > a)
     .Count(x => x);
 
+static int CountWindowIncreases(int windowSize, IEnumerable<int> depths) => depths
+    .Window(windowSize)
+    .Pairwise((a, b) => b.Sum() > a.Sum())
+    .Count(x => x);
