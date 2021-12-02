d82d92e370c62a8642ed6cd34a4cbab34fb5e2c1
Jon Fuller <jcfuller@sep.com>
2021-12-02 12:09:50 -0500

    day 2 - implemented part 1 navigation

--- Aoc2021/Day02.cs
+++ Aoc2021/Day02.cs
@@ -5,6 +5,29 @@ public class Day02 : ICommand
 {
     public async ValueTask ExecuteAsync(IConsole console)
     {
-        console.Output.WriteLine("day-02");
+        var instructions = File.ReadAllLines("input/02_sample").Select(ParseInstructionLine);
+
+        var currentPosition = new Position(0, 0);
+        foreach(var instruction in instructions)
+        {
+            currentPosition = instruction.instruction switch
+            {
+                "forward" => currentPosition with { x = currentPosition.x + instruction.amount },
+                "down" => currentPosition with { y = currentPosition.y + instruction.amount },
+                "up" => currentPosition with { y = currentPosition.y - instruction.amount },
+                _ => throw new Exception($"Invalid instruction: {instruction.instruction}")
+            };
+        }
+        console.Output.WriteLine(currentPosition);
+
     }
+
+    static (string instruction, int amount) ParseInstructionLine(string line)
+    {
+        var split = line.Split(" ");
+        return (split[0], int.Parse(split[1]));
+    }
+
+    record Position(int x, int y){}
 }
+
