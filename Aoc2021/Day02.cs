51bf7f6089a67aa801f6dd50c435756383ffbd85
Jon Fuller <jcfuller@sep.com>
2021-12-02 16:12:35 -0500

    day 2 - refactoring

--- Aoc2021/Day02.cs
+++ Aoc2021/Day02.cs
@@ -5,8 +5,8 @@ public class Day02 : ICommand
 {
     public async ValueTask ExecuteAsync(IConsole console)
     {
-        // PartA("input/02_sample");
-        // PartA("input/02");
+        PartA("input/02_sample");
+        PartA("input/02");
         PartB("input/02_sample");
         PartB("input/02");
 
@@ -14,7 +14,7 @@ public class Day02 : ICommand
         {
             var instructions = File.ReadAllLines(filename).Select(ParseInstructionLine);
             var finalPosition = Navigate(new Position(0, 0), instructions);
-            console.Output.WriteLine(filename);
+            console.Output.WriteLine($"Part A - {filename}");
             console.Output.WriteLine(finalPosition);
             console.Output.WriteLine(finalPosition.x * finalPosition.y);
         }
@@ -22,50 +22,42 @@ public class Day02 : ICommand
         void PartB(string filename)
         {
             var instructions = File.ReadAllLines(filename).Select(ParseInstructionLine);
-            var finalPosition = Navigate2(new SubConfig(new Position(0, 0), 0), instructions);
-            console.Output.WriteLine(filename);
-            console.Output.WriteLine(finalPosition);
-            console.Output.WriteLine(finalPosition.x * finalPosition.y);
+            var finalConfig = Navigate2(new SubConfig(new Position(0, 0), 0), instructions);
+            console.Output.WriteLine($"Part B - {filename}");
+            console.Output.WriteLine(finalConfig);
+            console.Output.WriteLine(finalConfig.pos.x * finalConfig.pos.y);
         }
 
-        static Position Navigate(Position currentPosition, IEnumerable<(string direction, int amount)> instructions)
-        {
-            foreach(var instruction in instructions)
-            {
-                currentPosition = instruction.direction switch
+        static Position Navigate(Position currentPosition, IEnumerable<Instruction> instructions) => instructions
+            .Aggregate(currentPosition,
+                (pos, instruction) => instruction.direction switch
                 {
-                    "forward" => currentPosition with { x = currentPosition.x + instruction.amount },
-                    "down" => currentPosition with { y = currentPosition.y + instruction.amount },
-                    "up" => currentPosition with { y = currentPosition.y - instruction.amount },
+                    "forward" => pos with { x = pos.x + instruction.amount },
+                    "down" => pos with { y = pos.y + instruction.amount },
+                    "up" => pos with { y = pos.y - instruction.amount },
                     _ => throw new Exception($"Invalid instruction: {instruction.direction}")
-                };
-            }
-            return currentPosition;
-        }
+                });
 
-        static Position Navigate2(SubConfig config, IEnumerable<(string direction, int amount)> instructions)
-        {
-            foreach(var instruction in instructions)
-            {
-                config = instruction.direction switch
+        static SubConfig Navigate2(SubConfig config, IEnumerable<Instruction> instructions) => instructions
+            .Aggregate(config,
+                (cfg, instruction) => instruction.direction switch
                 {
-                    "forward" => config with { pos = new Position(config.pos.x + instruction.amount, config.pos.y + (config.aim * instruction.amount)) },
-                    "down" => config with { aim = config.aim + instruction.amount },
-                    "up" => config with { aim = config.aim - instruction.amount },
+                    "forward" => cfg with {
+                        pos = new Position(cfg.pos.x + instruction.amount, cfg.pos.y + (cfg.aim * instruction.amount)) },
+                    "down" => cfg with { aim = cfg.aim + instruction.amount },
+                    "up" => cfg with { aim = cfg.aim - instruction.amount },
                     _ => throw new Exception($"Invalid instruction: {instruction.direction}")
-                };
-            }
-            return config.pos;
-        }
+                });
     }
 
-    static (string direction, int amount) ParseInstructionLine(string line)
+    static Instruction ParseInstructionLine(string line)
     {
         var split = line.Split(" ");
-        return (split[0], int.Parse(split[1]));
+        return new Instruction(split[0], int.Parse(split[1]));
     }
 
-    record SubConfig(Position pos, int aim){}
-    record Position(int x, int y){}
+    record Instruction(string direction, int amount);
+    record SubConfig(Position pos, int aim);
+    record Position(int x, int y);
 }
 
