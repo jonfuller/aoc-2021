feff29ef361c82b204aa77759a08e373db8378e3
Jon Fuller <jcfuller@sep.com>
2021-12-02 11:57:20 -0500

    added day 2 file and template file for other days

---
+++ Aoc2021/Day02.cs
@@ -0,0 +1,10 @@
+﻿namespace Aoc2021;
+
+[Command(name: "day-02")]
+public class Day02 : ICommand
+{
+    public async ValueTask ExecuteAsync(IConsole console)
+    {
+        console.Output.WriteLine("day-02");
+    }
+}
---
+++ Aoc2021/_DayN.cs
@@ -0,0 +1,10 @@
+﻿namespace Aoc2021;
+
+[Command(name: "day-N")]
+public class DayN : ICommand
+{
+    public async ValueTask ExecuteAsync(IConsole console)
+    {
+        console.Output.WriteLine(this.GetType().Name);
+    }
+}
