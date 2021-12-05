c5c063749dc60cfce45474e7424932aea1abcea0
Jon Fuller <jcfuller@sep.com>
2021-12-05 08:29:21 -0500

    day 5 - parts 1 & 2

--- Aoc2021/Day05.cs
+++ Aoc2021/Day05.cs
@@ -5,10 +5,31 @@ public class Day05 : ICommand
 {
     public async ValueTask ExecuteAsync(IConsole console)
     {
-        var lineSegments = File.ReadAllLines("input/05_sample").Select(ParseLine).ToArray();
+        var sampleLines = ParseLineSegments("input/05_sample");
+        var sampleStraightLines = sampleLines
+            .Where(l => l.IsHorizontal || l.IsVertical)
+            .ToList();
+        console.Output.WriteLine(MultipleOverlaps(sampleStraightLines));
+        console.Output.WriteLine(MultipleOverlaps(sampleLines));
 
-        lineSegments.ForEach(l => console.Output.WriteLine(l));
+        var lines = ParseLineSegments("input/05");
+        var straightLines = lines
+            .Where(l => l.IsHorizontal || l.IsVertical)
+            .ToList();
+        console.Output.WriteLine(MultipleOverlaps(straightLines));
+        console.Output.WriteLine(MultipleOverlaps(lines));
     }
+
+    static int MultipleOverlaps(IEnumerable<LineSegment> lineSegments) => lineSegments
+            .SelectMany(line => line.Points)
+            .GroupBy(p => p)
+            .Where(p => p.Count() > 1)
+            .Count();
+
+    static List<LineSegment> ParseLineSegments(string filename) => File
+        .ReadAllLines(filename)
+        .Select(ParseLine)
+        .ToList();
 
     static LineSegment ParseLine(string rawLine)
     {
@@ -24,6 +45,46 @@ public class Day05 : ICommand
 
     record LineSegment(Point start, Point end)
     {
+        public bool IsVertical => start.y == end.y;
+        public bool IsHorizontal => start.x == end.x;
+
+        public List<Point> Points
+        {
+            get
+            {
+                if (IsVertical)
+                {
+                    return EnumerableX.RangeFrom(start.x, end.x).Select(x => new Point(x, start.y)).ToList();
+                }
+                else if (IsHorizontal)
+                {
+                    return EnumerableX.RangeFrom(start.y, end.y).Select(y => new Point(start.x, y)).ToList();
+                }
+                else
+                {
+                    var xs = EnumerableX.RangeFrom(start.x, end.x);
+                    var ys = EnumerableX.RangeFrom(start.y, end.y);
+
+                    return xs.Zip(ys, (x, y) => new Point(x, y)).ToList();
+                }
+            }
+        }
     }
     record Point(int x, int y);
+
+    public static class EnumerableX
+    {
+        public static IEnumerable<int> RangeFrom(int start, int end)
+        {
+            if (start == end)
+                return new[] {start};
+
+            if (start < end)
+            {
+                return Enumerable.Range(start, end-start+1);
+            }
+
+            return Enumerable.Range(end, start - end + 1).Reverse().ToList();
+        }
+    }
 }
