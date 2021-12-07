2113fda2cdb6fbcd6cf7321a52ef514c74bc1bb3
Jon Fuller <jcfuller@sep.com>
2021-12-07 16:19:15 -0500

    day 7 - part 1, scaffolding for part 2

---
+++ Aoc2021/Day07.cs
@@ -0,0 +1,33 @@
+﻿namespace Aoc2021;
+
+[Command(name: "day-07")]
+public class Day07 : ICommand
+{
+    public async ValueTask ExecuteAsync(IConsole console)
+    {
+        console.Output.WriteLine(CheapestFuelToSamePosition("input/07_sample", TravelA));
+        console.Output.WriteLine(CheapestFuelToSamePosition("input/07", TravelA));
+        console.Output.WriteLine(CheapestFuelToSamePosition("input/07_sample", TravelB));
+        console.Output.WriteLine(CheapestFuelToSamePosition("input/07", TravelB));
+    }
+
+    static int CheapestFuelToSamePosition(string inputFile, Func<int, int, int> fuelCalculator)
+    {
+        var positions = File.ReadAllText(inputFile).Split(",").Select(int.Parse).ToList();
+
+        var max = positions.Max();
+        var min = positions.Min();
+
+        return EnumerableX
+            .RangeFrom(min, max)
+            .Select(depth => (depth, totalFuel: positions.Select(position => fuelCalculator(position, depth)).Sum()))
+            .MinBy(keySelector: f => f.totalFuel)
+            .totalFuel;
+    }
+
+    static int TravelA(int currentDepth, int destinationDepth) => Math.Abs(currentDepth - destinationDepth);
+    static int TravelB(int currentDepth, int destinationDepth)
+    {
+        return 0;
+    }
+}
